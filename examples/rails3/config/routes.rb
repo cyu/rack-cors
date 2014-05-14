cd5464a2d3f2f5c5d5451fd66bc5cb6dc02dc430
Calvin Yu <calvin@scoutmob.com>
2014-05-14 13:59:41 -0400

    Return Content-Type header in example

--- examples/rails3/config/routes.rb
+++ examples/rails3/config/routes.rb
@@ -1,4 +1,4 @@
 Rails3::Application.routes.draw do
   resources :cors
-  root :to => proc {|env| [200, {}, ["Hello world"]] }
+  root :to => proc {|env| [200, {'Content-Type' => 'text/plain'}, ["Hello world"]] }
 end
