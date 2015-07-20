4267a38ccd57c6f302aecf31266d8479ba545d40
Michael <mmadrid99@gmail.com>
2015-07-19 23:09:11 -0400

    Add patch to resource methods

--- examples/rails4/config/application.rb
+++ examples/rails4/config/application.rb
@@ -32,7 +32,7 @@ module Rails4
 
         resource '*',
           :headers => :any,
-          :methods => [:get, :post, :delete, :put, :options, :head],
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
           :max_age => 0
       end
     end
