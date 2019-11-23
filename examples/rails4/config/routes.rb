dc58f04916248536b4ffedb068edfb8505d1720e
Calvin Yu <me@sourcebender.com>
2019-11-23 17:25:39 -0500

    Remove Rails 4 example

--- examples/rails4/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :cors
-  match '/', :to => proc {|env| [200, {'Content-Type' => 'text/plain'}, ["Hello world"]] },
-             :via => [:get, :post, :put, :delete, :options, :head, :patch]
-end
