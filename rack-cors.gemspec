cyu/rack-cors

commit 41ccfee6811389fadcf48f9a00a634389c78a740

Various docs for 2.0.2 (#275)
* Doc: 2.0.2: Add CVE ids to changelog

* Doc: Add changelog to rubygems dot org page

This should add a changelog link in the lower right corner of
the rubygems dot org page.

* Add .idea to .gitignore

.idea is the RubyMine configuration directory

rack-cors.gemspec
@@ -19,6 +19,10 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
+  s.metadata = {
+    'changelog_uri' => 'https://github.com/cyu/rack-cors/blob/master/CHANGELOG.md'
+  }
+
   spec.add_dependency 'rack', '>= 2.0.0'
   spec.add_development_dependency 'bundler', '>= 1.16.0', '< 3'
   spec.add_development_dependency 'minitest', '~> 5.11.0'

.gitignore
@@ -2,6 +2,7 @@
 *.rbc
 .bundle
 .config
+.idea
 .yardoc
 /Gemfile.lock
 InstalledFiles

commit 80ae4c47b64693aa6b3db2355c768e8829fddae3

Fix error loading gemspec
Installing dependencies has been failing since the commit 41ccfee introduced
an undefined local variable in the gemspec:

```
[!] There was an error parsing `Gemfile`:
[!] There was an error while loading `rack-cors.gemspec`: undefined local variable or method `s' for main:Object. Bundler cannot continue.

 #  from /home/runner/work/rack-cors/rack-cors/rack-cors.gemspec:22
 #  -------------------------------------------
 #
 >    s.metadata = {
 #      'changelog_uri' => 'https://github.com/cyu/rack-cors/blob/master/CHANGELOG.md'
 #  -------------------------------------------
. Bundler cannot continue.

 #  from /home/runner/work/rack-cors/rack-cors/Gemfile:6
 #  -------------------------------------------
 #  # Specify your gem's dependencies in rack-cors.gemspec
 >  gemspec
 #
 #  -------------------------------------------
```

rack-cors.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
-  s.metadata = {
+  spec.metadata = {
     'changelog_uri' => 'https://github.com/cyu/rack-cors/blob/master/CHANGELOG.md'
   }
 